f8dade44d737af534f13e39bec9e20e6faac59eb
solos-pro <sergey-solonichenko@rambler.ru>
2022-02-10 18:31:22 +0300

    database.py with SQLAlchemy

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from app.config import Config
+from flask_restx import Api
+from app.database import db
 
 def create_app(config: Config) -> Flask:
-    pass
+    application = Flask(__name__)
+    application.config.from_object(config)
+    application.app_context().push()
 
 def configure_app(application: Flask):
-    pass
+    db.init_app(application)
+    api = Api(app)
+    api.add_namespace(movies_ns)
+    api.add_namespace(genres_ns)
+    api.add_namespace(directors_ns)
 
 if __name__ == '__main__':
     app_config = Config()
