bd055209e3d633be57baf6f943e7990bf8d6c0ca
solos-pro <sergey-solonichenko@rambler.ru>
2022-02-26 10:19:35 +0300

    added hash-salt and num of iterations in config.py

--- app/config.py
+++ app/config.py
@@ -4,3 +4,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///./test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
+PWD_HASH_SALT = b'secret here'
+PWD_HASH_ITERATIONS = 100_000
